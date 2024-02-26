07ea54aab2babfdba40889c60bba10acfd04759e
CircleD5 <144087288+CircleD5@users.noreply.github.com>
2024-02-26 13:15:02 +0900

    Fix auto_channel name

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import time
 from module.parser import parse_generation_parameters
 
 # Configuration Parameter
-auto_channel = 'prompts-auto-share'
+auto_channel = '🤖│prompts-auto-share'
 
 #####################
 
